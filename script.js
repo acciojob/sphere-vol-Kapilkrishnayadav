50fde699af5f6e1e2e768dcb918e519a1e7fe37a
Kapil Krishna Yadav <93436537+Kapilkrishnayadav@users.noreply.github.com>
2025-05-29 10:58:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(e) {
+    e.preventDefault();
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+    const radius = Number(document.getElementById("radius").value);
+    const v = document.getElementById("volume");
+
+    if (isNaN(radius) || radius < 0) {
+        v.value = "NaN";
+        return;
+    }
+
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    v.value = volume.toFixed(4);
+}
+
+window.onload = function() {
+    document.getElementById('MyForm').onsubmit = volume_sphere;
+    document.getElementById('volume').value = "";  // clear volume input on load
+};
